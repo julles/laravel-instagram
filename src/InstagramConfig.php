a090589b5f45707dc8a116a7d1f3c5882a55900d
Muhamad Reza Abdul Rohim <reza.wikrama2@gmail.com>
2016-01-16 02:28:43 +0700

    update

--- src/InstagramConfig.php
+++ src/InstagramConfig.php
@@ -3,6 +3,6 @@
 	// masukan user id dan akses token instagram yang akan manage
 
 	return [
-		'userId' => '1619689047',
-		'accessToken' => '1619689047.1677ed0.789fdce9420a4946a35e1cdd16f9c653', 
+		'userId' => 'user-id-kamu',
+		'accessToken' => 'access-token-kamu', 
 	];
