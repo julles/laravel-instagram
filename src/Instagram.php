dba87a697768798f228520e2e6175c56f9bf95ff
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-01-20 03:24:23 +0700

    tambah helper login instagram

--- src/Instagram.php
+++ src/Instagram.php
@@ -1,5 +1,11 @@
 <?php namespace Oblagio\Instagram;
 
+/**
+ * Laravel Instagram Package , Indonesia Punya 
+ *
+ * @author   Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
+ */
+
 class Instagram
 
 {	
@@ -8,12 +14,24 @@ class Instagram
 	
 	protected $accessToken;
 
+	protected $clientId;
+
+	protected $clientSecret;
+
+	protected $redirectUri;
+
 	public function __construct()
 
 	{
 		$this->userId = config('config.userId');
 
 		$this->accessToken = config('config.accessToken');
+	
+		$this->clientId = config('config.clientId');
+
+		$this->clientSecret = config('config.clientSecret');
+
+		$this->redirectUri = config('config.redirectUri');
 	}
 
 	public function getContents($url)
@@ -149,6 +167,57 @@ class Instagram
 	{
 		$url = 'https://api.instagram.com/v1/users/self/followed-by?access_token='.$this->accessToken;
 		return $this->getContents($url);
+	}
+
+	public function getCodeAuth()
+
+	{
+		$url = "https://api.instagram.com/oauth/authorize/?client_id=".$this->clientId."&redirect_uri=".$this->redirectUri."&response_type=code";
+		return redirect($url);
+	}
+
+	public function curlAuth($code)
+
+	{
+
+		$url = "https://api.instagram.com/oauth/access_token";
+
+		$ch = curl_init();
+
+		$info = [
+
+			'client_id' => $this->clientId,
+			'client_secret' => $this->clientSecret,
+			'grant_type' => 'authorization_code',
+			'redirect_uri' => $this->redirectUri,
+			'grant_type' => 'authorization_code',
+			'code' => $code,
+		];
+
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $info);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  //to suppress the curl output
+		$result = json_decode(curl_exec($ch) , true);
+		curl_close ($ch);
+		
+		return $result;
+
+	}
+
+	public function auth($code)
+
+	{
+		$auth = $this->curlAuth($code);
+		
+		$jadikan_flatt_array = array_flatten($auth);
+		$masukan_key = ['access_token' , 'username' , 'bio' , 'website' , 'pic' , 'full_name', 'id'];
+		
+		$hasil = array_combine($masukan_key, $jadikan_flatt_array);
+
+		return $hasil;
 	}
 
 }
