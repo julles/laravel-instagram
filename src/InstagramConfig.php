d653196eb70b088d07ea89f3034e58afb3d9f831
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-01-20 03:21:05 +0700

    tambah attribut konfigurasi

--- src/InstagramConfig.php
+++ src/InstagramConfig.php
@@ -1,8 +1,14 @@
 <?php
-
-	// masukan user id dan akses token instagram yang akan manage
-
+/* 	NOTES
+ *  Jika hanya menampilkan data pribadi saja , cukup masukan userId dan accesToken
+ *  Jika diperlukan autentikasi login semua nya wajib di isi 
+ */
+	
 	return [
 		'userId' => 'user-id-kamu',
-		'accessToken' => 'access-token-kamu', 
+		'accessToken' => 'access-token-kamu',
+		
+		'clientId' => 'client-id-kamu',
+		'clientSecret' => 'client-secrets-kamu',
+		'redirectUri' => 'redirect-uri-kamu',
 	];
